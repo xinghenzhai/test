6391dfe716b0c1a85b14c6bb3e65c23804d6c56d
我 <396953968@qq.com>
2015-10-31 09:54:13 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by 李新奇 on 15/10/31.
+//  Copyright © 2015年 李新奇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
